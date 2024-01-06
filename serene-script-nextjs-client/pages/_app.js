aeb0f9a81123840d4b44d26cd636344d55166263
Kaiden Flaig <kaidenflaig335@gmail.com>
2024-01-06 15:56:10 -0700

    added theme provider

--- serene-script-nextjs-client/pages/_app.js
+++ serene-script-nextjs-client/pages/_app.js
@@ -1,5 +1,18 @@
-import '@/styles/globals.css'
+import { ThemeProvider } from "@emotion/react";
+import "../styles/styles.css";
+import { CssBaseline, createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+    },
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </ThemeProvider>
+    );
 }
