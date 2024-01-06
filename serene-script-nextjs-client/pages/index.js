733fcdfad5ed7cfe7520ba9a32a30ccae450bfac
Reonel Duque <rduque89@yahoo.com>
2024-01-06 13:52:21 -0700

    Update index.js

--- serene-script-nextjs-client/pages/index.js
+++ serene-script-nextjs-client/pages/index.js
@@ -23,13 +23,11 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <Body>
                 <main className={`${styles.main} ${inter.className}`}>
                     <div>
                         <h1>Serene Script</h1>
                     </div>
                 </main>
-            </Body>
 
         </>
     );
