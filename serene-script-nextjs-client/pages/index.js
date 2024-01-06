9472cc70bba56ec8a14b14e2c81182e8542f1c80
Reonel Duque <rduque89@yahoo.com>
2024-01-06 13:50:59 -0700

    hopefully this doesn't screw anything up. mui installed

--- serene-script-nextjs-client/pages/index.js
+++ serene-script-nextjs-client/pages/index.js
@@ -23,11 +23,14 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main className={`${styles.main} ${inter.className}`}>
-                <div>
-                    <h1>Serene Script</h1>
-                </div>
-            </main>
+            <Body>
+                <main className={`${styles.main} ${inter.className}`}>
+                    <div>
+                        <h1>Serene Script</h1>
+                    </div>
+                </main>
+            </Body>
+
         </>
     );
 }
