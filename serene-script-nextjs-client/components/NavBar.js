1abe95b21b94d319f42eada2ccf13377bcbaf72e
Meghan Laberge <meghanelaberge@gmail.com>
2024-01-06 17:02:07 -0700

    added leaf

--- serene-script-nextjs-client/components/NavBar.js
+++ serene-script-nextjs-client/components/NavBar.js
@@ -6,6 +6,8 @@ import Typography from "@mui/material/Typography";
 import Menu from "@mui/material/Menu";
 import Box from "@mui/material/Box"
 import { useTheme } from "@mui/material";
+import SpaIcon from '@mui/icons-material/Spa';
+
 
 export default function NavBar(props) {
     const theme = useTheme();
@@ -16,7 +18,7 @@ export default function NavBar(props) {
                 <Toolbar>
                     <Link href="/" /*insert reference page here*/>
                         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                            Home
+                            <SpaIcon/>
                         </Typography>
                     </Link>
                     <Link href="/chat">
